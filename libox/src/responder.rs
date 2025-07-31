eafbb8933a8622f7db26b492682d1ee2d9a8d84e
Ed Chapman - Turing <edchapmanelc@gmail.com>
2025-07-31 10:33:53 +0100

    replace with read function and fix path

--- libox/src/responder.rs
+++ libox/src/responder.rs
@@ -1,23 +1,13 @@
 use crate::drcore::log;
 use std::env;
-use std::fs::File;
-use std::io::Read;
+use std::fs::read;
 
 pub fn respond(request: Vec<u8>) -> Vec<u8> {
     let input_file_path = env::var("FUZZ_INPUT").expect("FUZZ_INPUT must be set as an environment variable, with a file path to the fuzz input file: the file that AFL will write the mutated fuzz input to.");
 
     log(&format!("request={:?}", request));
 
-    let mut f = File::open("foo.txt").expect(&format!(
-        "Failed to read FUZZ_INPUT file at path: {}",
-        input_file_path,
-    ));
-
-    let mut response = Vec::new();
-    f.read_to_end(&mut response).expect(&format!(
-        "Failed reading bytes from file: {}",
-        input_file_path
-    ));
+    let response = read(input_file_path).unwrap();
 
     log(&format!("request={:?} -> response={:?}", request, response));
     response
