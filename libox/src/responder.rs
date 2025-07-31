bcda8caf63fd9ffbea6b44715091e1a7d4dec7f6
Ed Chapman - Turing <edchapmanelc@gmail.com>
2025-07-31 10:28:40 +0100

    add log before file read

--- libox/src/responder.rs
+++ libox/src/responder.rs
@@ -6,6 +6,8 @@ use std::io::Read;
 pub fn respond(request: Vec<u8>) -> Vec<u8> {
     let input_file_path = env::var("FUZZ_INPUT").expect("FUZZ_INPUT must be set as an environment variable, with a file path to the fuzz input file: the file that AFL will write the mutated fuzz input to.");
 
+    log(&format!("request={:?}", request));
+
     let mut f = File::open("foo.txt").expect(&format!(
         "Failed to read FUZZ_INPUT file at path: {}",
         input_file_path,
