b4ac749a8e6ad673b5e09d905541328146fc1c3e
Ed Chapman - Turing <edchapmanelc@gmail.com>
2025-08-14 13:56:14 +0100

    mutator with energy

--- mutator/src/lib.rs
+++ mutator/src/lib.rs
@@ -1,44 +1,73 @@
-use libc::{c_char, c_uchar, c_uint, c_void, free, malloc, memcpy};
+use std::{fs::OpenOptions, io::Write, slice};
+
+use libc::{c_char, c_uchar, c_uint, c_void};
+
+const NO_FUTHER_MUTATIONS: u8 = 1;
+
+pub type WriteToTestCaseFn = extern "C" fn(*mut c_void, c_uint);
+pub type RunTargetFn = extern "C" fn(*mut *mut c_char, c_uint) -> c_uchar;
+
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn dll_trim_testcase(
+    _len: *mut c_uint,
+    _exec_cksum: c_uint,
+    _an_buf: *mut c_uchar,
+    _trace_bits: *mut c_uchar,
+    _write_to_testcase: WriteToTestCaseFn,
+    _run_target: RunTargetFn,
+    _argv: *mut *mut c_char,
+    _exec_timeout: c_uint,
+) -> c_uchar {
+    // Indicate that no write is needed, buffer remains uncahnged.
+    0
+}
 
 pub type CommonFuzzStuffFn =
     Option<unsafe extern "C" fn(argv: *mut *mut c_char, buf: *mut c_uchar, len: c_uint) -> c_uchar>;
 
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn dll_mutate_testcase(
+pub unsafe extern "C" fn dll_mutate_testcase_with_energy(
     argv: *mut *mut c_char,
     buf: *mut c_uchar,
     len: c_uint,
+    energy: c_uint,
     common_fuzz_stuff: CommonFuzzStuffFn,
 ) -> c_uchar {
     unsafe {
-        let mut bailout: c_uchar = 1;
-
         if buf.is_null() || len == 0 {
-            return bailout;
-        }
+            return NO_FUTHER_MUTATIONS;
+        };
 
-        // allocate new buffer
-        let newbuf = malloc(len as usize) as *mut c_uchar;
-        if newbuf.is_null() {
-            return bailout;
-        }
+        let slice = slice::from_raw_parts(buf, len as usize);
 
-        // copy original
-        memcpy(newbuf as *mut c_void, buf as *const c_void, len as usize);
+        let mut log_f = OpenOptions::new()
+            .create(true)
+            .append(true)
+            .open("mutator_log.txt")
+            .expect("Failed to create or open mutator_log.txt");
+
+        writeln!(log_f, "{:?} has energy {}", slice, energy)
+            .expect("Failed to write to mutation_log.txt file");
+
+        let focus_len = match energy {
+            // focus on the first 20% of the bytes when energy is low.
+            0..256 => (0.2 * len as f32).floor() as usize,
+            _ => len as usize,
+        };
 
         // mutate each byte
-        for i in 0..len {
-            *newbuf.add(i as usize) = (*newbuf.add(i as usize)).wrapping_add(1);
+        for i in 0..focus_len {
+            let mut mutated = slice.to_owned();
+            mutated[i] = mutated[i].wrapping_add(1);
 
             if let Some(common_fuzz) = common_fuzz_stuff {
-                if common_fuzz(argv, newbuf, len) != 0 {
-                    bailout = 1;
+                if common_fuzz(argv, mutated.as_ptr() as *mut u8, len) != 0 {
+                    // signal from afl to stop mutating this input
                     break;
                 }
             }
         }
 
-        free(newbuf as *mut c_void);
-        bailout
+        return NO_FUTHER_MUTATIONS;
     }
 }
