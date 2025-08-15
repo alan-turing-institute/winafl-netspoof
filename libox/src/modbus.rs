8ff2a250cf53de2e6067badc661823a7ea7d1cdb
Ed Chapman - Turing <edchapmanelc@gmail.com>
2025-08-15 17:02:58 +0100

    better modbus error handling

--- libox/src/modbus.rs
+++ libox/src/modbus.rs
@@ -6,56 +6,63 @@ use modbus_core::tcp::{RequestAdu, server::decode_request};
 use modbus_core::{Coils, Request, Response, ResponsePdu};
 
 pub fn respond(request: Vec<u8>) -> Result<Vec<u8>, String> {
-    let mut buf = vec![0u8; 256];
+    let request_payload = decode_request(&request)
+        .map_err(|e| format!("Failed to decode Modbus payload from TCP request: {e}"))?;
 
-    let mut response = vec![0u8; 256];
+    // Parse the Request ADU.
+    let RequestAdu { hdr, pdu } =
+        request_payload.ok_or("Failed parsing request payload as Modbus".to_string())?;
 
-    match decode_request(&request) {
-        Ok(data) => {
-            let RequestAdu { hdr, pdu } = data.expect("Failed parsing request payload as Modbus");
-            let response_pdu = match pdu.0 {
-                Request::ReadCoils(addr, n_coils) => {
-                    log(&format!(
-                        "[modbus] REQUEST: read {} coils @ 0x{:04X}",
-                        n_coils, addr
-                    ));
-                    // 1 bit per coil, 8 coils per byte
-                    let content_len: usize = n_coils.div_ceil(8).into();
-                    let content = fuzzer::call();
-                    if content.len() != content_len {
-                        log(&format!(
-                            "[modbus] Fuzzer content wasn't expect length of {content_len}!"
-                        ));
-                    }
-                    let mut coils = Vec::with_capacity(content_len);
-                    modbus_core::unpack_coils(&content, n_coils, &mut coils)
-                        .map_err(|e| format!("Failed to unpack coils: {e}"))?;
-                    buf.truncate(content_len);
-                    Ok(Response::ReadCoils(
-                        Coils::from_bools(&coils, &mut buf).unwrap(),
-                    ))
-                }
-                Request::WriteSingleCoil(addr, coil) => {
-                    log(&format!(
-                        "[modbus] REQUEST: write single coil: ={} @ 0x{:04X}",
-                        coil as u8, addr
-                    ));
-                    Ok(Response::WriteSingleCoil(addr))
-                }
-                _ => todo!("other request types"),
-            };
-            log(&format!("[modbus] RESPONSE: {:?}", response_pdu.unwrap()));
-            let response_adu = ResponseAdu {
-                hdr,
-                pdu: ResponsePdu(response_pdu),
-            };
-            encode_response(response_adu, &mut response)
-                .map(|n_bytes_written| {
-                    response.truncate(n_bytes_written);
-                    response
-                })
-                .map_err(|e| format!("Failed encoding the response: {e}"))
+    // Assemble a Response PDU by matching on the request type.
+    let mut response_pdu_buf = vec![0u8; 256];
+    let response_pdu = match pdu.0 {
+        Request::ReadCoils(addr, n_coils) => {
+            log(&format!(
+                "[modbus] REQUEST: read {} coils @ 0x{:04X}",
+                n_coils, addr
+            ));
+            // 1 bit per coil, 8 coils per byte
+            let content_len: usize = n_coils.div_ceil(8).into();
+            let content = fuzzer::call();
+            if content.len() != content_len {
+                log(&format!(
+                    "[modbus] Fuzzer content wasn't expect length of {content_len}!"
+                ));
+            }
+            // Initialise a coil array with dummy values.
+            let mut coils = vec![false; content_len];
+            // Unpack the bytes from the fuzzer into the coil array.
+            modbus_core::unpack_coils(&content, n_coils, &mut coils)
+                .map_err(|e| format!("Failed to unpack coils: {e}"))?;
+
+            response_pdu_buf.truncate(content_len);
+            Ok(Response::ReadCoils(
+                Coils::from_bools(&coils, &mut response_pdu_buf).unwrap(),
+            ))
         }
-        Err(_) => todo!("handle error decoding request"),
-    }
+        Request::WriteSingleCoil(addr, coil) => {
+            log(&format!(
+                "[modbus] REQUEST: write single coil: ={} @ 0x{:04X}",
+                coil as u8, addr
+            ));
+            Ok(Response::WriteSingleCoil(addr))
+        }
+        _ => todo!("other request types"),
+    };
+
+    log(&format!(
+        "[modbus] RESPONSE PDU: {:?}",
+        response_pdu.unwrap()
+    ));
+    let response_adu = ResponseAdu {
+        hdr,
+        pdu: ResponsePdu(response_pdu),
+    };
+    let mut response = vec![0u8; 256];
+    encode_response(response_adu, &mut response)
+        .map(|n_bytes_written| {
+            response.truncate(n_bytes_written);
+            response
+        })
+        .map_err(|e| format!("Failed encoding the response: {e}"))
 }
